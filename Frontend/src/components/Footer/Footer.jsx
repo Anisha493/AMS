09ce57e76e569e460bdf175769c2679753f2d1df
Anisha493 <karkianisha84@gmail.com>
2025-07-09 09:52:34 +0545

    Update Footer.jsx

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,6 @@ const Footer = () => {
   return (
     <footer className="bg-gray-500 text-white py-6 mt-8">
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-8">
-      
         <div>
           <h2 className="text-lg font-semibold mb-2">About</h2>
           <p className="text-sm">
@@ -11,8 +10,6 @@ const Footer = () => {
           </p>
           <p className="text-sm">Thank you for visiting, and feel free to <a href="contact">contact</a> with us!</p>
         </div>
-
-        
         <div>
           <h2 className="text-lg font-semibold mb-2">Quick Links</h2>
           <ul className="space-y-1 text-sm">
@@ -22,8 +19,6 @@ const Footer = () => {
             <li><a href="/signIn" className="hover:underline">Login</a></li>
           </ul>
         </div>
-
-       
         <div>
           <h2 className="text-lg font-semibold mb-2">Contact Us</h2>
           <p className="text-sm">Email: support@attendanceapp.com</p>
@@ -31,8 +26,6 @@ const Footer = () => {
           <p className="text-sm">Address: Itahari, Nepal</p>
         </div>
       </div>
-
-    
       <div className="text-center mt-6 text-sm text-gray-300">
         © {new Date().getFullYear()} StudentConnectNepalApp. All rights reserved.
       </div>
